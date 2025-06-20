0e81f144fcc4950631c0637211644eb3ae4ee7f5
Neoorass <songjihan45@gmail.com>
2025-06-20 18:02:49 +0900

    monster_coin_test1

---
+++ coin.py
@@ -0,0 +1,7 @@
+import random
+import monster_data
+
+monster_data.monster_result
+cal = (monster_data.monster_result[1])/2
+
+coin = random.randrange(cal,cal + 50) #돈
---
+++ monster_data.py
@@ -0,0 +1,24 @@
+import random
+
+
+monster = random.randrange(0,4) #5개 중에 랜덤으로
+
+if (monster == 0):
+    
+    monster_result = ['몹1',100,10,5]
+
+elif (monster == 1):
+    
+    monster_result = ['몹2',100,10,5]
+    
+elif (monster == 2):
+    
+    monster_result = ['몹3',100,10,5]
+    
+elif (monster == 3):
+    
+    monster_result = ['몹4',100,10,5]
+    
+elif (monster == 4):
+    
+    monster_result = ['몹5',100,10,5]
